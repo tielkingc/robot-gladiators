eeb8420bb6eb326f3ea9177d887c4c0246bee727
tielkingc <tielking5@gmail.com>
2021-04-19 12:37:04 -0400

    can name robot

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,8 @@
-window.alert("This is an alert. JavaScript is working!")
+var playerName = window.prompt("What is your robot's name?");
+window.log(playerName);
+
+function fight () {
+    window.alert("The fight has begun!");
+}
+
+//fight()
